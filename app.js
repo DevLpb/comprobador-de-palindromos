62382af5f1e5d96c62cb95a94fd211bebb1df3fd
Leonel <leonelpiedrabuena04@gmail.com>
2024-03-04 13:23:51 -0300

    Refactorización de código
    Práctica de delegación de tareas en distintas funciones.

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const textInput = document.getElementById('text-input');
 const checkButton = document.getElementById('check-btn');
 const result = document.getElementById('result');
 
-checkButton.addEventListener('click', () => {
+// Bloque de código original. Sin delegar tareas //
+
+/* checkButton.addEventListener('click', () => {
     const regExp = /[a-z0-9]/gi;
     const textValue = textInput.value
     if (textValue === "") {
@@ -17,4 +19,35 @@ checkButton.addEventListener('click', () => {
         result.innerHTML = `<p><strong>${textValue}</strong> is not a palindrome.</p>`
     }
     }
+}) */
+
+// Refactorización para delegar tareas en distintas funciones //
+checkButton.addEventListener('click', () => {
+    isPalindrome(textInput.value);
 })
+
+function isEmpty(str) {
+    if (str === "") {
+        alert("Por favor, ingrese un valor");
+        return true;
+    }
+    return false;
+}
+
+function displayElements() {
+    textInput.value = "";
+    result.classList.remove('hidden');
+}
+
+function isPalindrome(str) {
+    if (isEmpty(str)) {
+        return
+    } else {
+        const regExp = /[a-z0-9]/gi;
+        let newArr = str.match(str).join("").toLowerCase();
+        displayElements();
+        newArr === newArr.split("").reverse().join("") ? 
+        result.innerHTML = `<p><strong>${str}</strong> is a palindrome.</p>` : 
+        result.innerHTML = `<p><strong>${str}</strong> is not a palindrome.</p>`;
+    }
+}
